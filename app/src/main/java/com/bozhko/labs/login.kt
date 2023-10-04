94668332c72f0decbc796731bbadc933aeb14323
patchouli <co0lthingstv1337@gmail.com>
2023-10-04 21:33:51 +0200

    initial

---
+++ app/src/main/java/com/bozhko/labs/login.kt
@@ -0,0 +1,11 @@
+package com.bozhko.labs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
